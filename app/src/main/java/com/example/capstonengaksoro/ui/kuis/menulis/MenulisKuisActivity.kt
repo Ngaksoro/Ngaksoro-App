b9c97371ae856aaa188c5625b16f47482b34da2d
Zeeus <airlanggapermana96@gmail.com>
2023-06-14 23:51:46 +0700

    Final Update Apps

--- app/src/main/java/com/example/capstonengaksoro/ui/kuis/menulis/MenulisKuisActivity.kt
+++ app/src/main/java/com/example/capstonengaksoro/ui/kuis/menulis/MenulisKuisActivity.kt
@@ -24,7 +24,7 @@ import com.example.capstonengaksoro.ui.ViewModelFactory
 import com.example.capstonengaksoro.utils.changeActivity
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.RequestBody
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 
@@ -212,10 +212,9 @@ class MenulisKuisActivity : AppCompatActivity() {
 
     private fun createMultipartFromBitmap(bitmap: Bitmap): MultipartBody.Part {
         val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream)
         val requestBody =
-            byteArrayOutputStream.toByteArray()
-                .toRequestBody("image/png".toMediaTypeOrNull(), 0, content.size)
+            RequestBody.create("image/png".toMediaTypeOrNull(), byteArrayOutputStream.toByteArray())
         return MultipartBody.Part.createFormData("file", "image.png", requestBody)
     }
 
